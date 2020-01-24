ea23b947ee707b8aa4408d384a9591d00986874d
rahulzura <rahuldesune@gmail.com>
2020-01-25 01:08:21 +0530

    fixed apt key

--- controllers/image.js
+++ controllers/image.js
@@ -1,7 +1,7 @@
-const Clarifai = require('clarifai');
+const Clarifai = require("clarifai");
 
 const app = new Clarifai.App({
- apiKey: API_CLARIFAI
+  apiKey: process.env.API_CLARIFAI
 });
 
 const handleApiCall = (req, res) => {
@@ -10,21 +10,22 @@ const handleApiCall = (req, res) => {
     .then(data => {
       res.json(data);
     })
-    .catch(err => res.status(400).json('unable to work with API'))
-}
+    .catch(err => res.status(400).json("unable to work with API"));
+};
 
 const handleImage = (req, res, db) => {
   const { id } = req.body;
-  db('users').where('id', '=', id)
-    .increment('entries', 1)
-    .returning('entries')
+  db("users")
+    .where("id", "=", id)
+    .increment("entries", 1)
+    .returning("entries")
     .then(entries => {
       res.json(entries[0]);
     })
-    .catch(err => res.status(400).json('unable to get entries'));
-}
+    .catch(err => res.status(400).json("unable to get entries"));
+};
 
 module.exports = {
   handleImage: handleImage,
   handleApiCall: handleApiCall
-}
+};
